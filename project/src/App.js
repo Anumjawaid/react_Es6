aef03182c2e9fe207c1d8009c7646e80f964ff9a
Anumjawaid <anumjawaid99@gmail.com>
2020-06-29 23:41:52 +0500

    Counter by using Hooks

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,7 @@ import Start1 from './class';
 import MSG from './hooksmessage'
 import HOOKS from './classhookmessage'
 import Counter from './counterclass'
+import Count from './hookcounter'
 function App() {
   
 
@@ -11,10 +12,20 @@ function App() {
   return (
     <div className="box">
       <My name=" functional class"/>
+      <h6>*************</h6>
+     
       <Start1 name=" Start class"/>
+      <h6>*************</h6>
+     
       <MSG/>
+      <h6>*************</h6>
+
       <HOOKS/>
+      <h6>*************</h6>
+
       <Counter/>
+      <h6>*************</h6>
+      <Count/>
 
      
     </div>
