dbf78c4a3922d667a60b2cab54902671ba9e8b81
Anumjawaid <anumjawaid99@gmail.com>
2020-10-01 23:40:09 +0500

    accesing appending

--- JsObj/app.js
+++ JsObj/app.js
@@ -2,6 +2,15 @@ obj={
     fname:'Anum',
     lname:'Jawaid',
     Age :21,
+    intro:function (){
+        //document.write("MY Name IS Anum") Normally we will do that
+        document.getElementById('q').innerHTML="MY Name IS Anum"
+        console .log("My name is Anum")
+    }
 }
-document.write("if we direct call obj ==> ",obj)
 console.log("if we direct call obj ==> ",obj)
+document.getElementById('o').innerHTML=("if we direct call obj ==> ",obj)
+console.log("accessing single name",obj.fname)
+document.getElementById('p').innerHTML=obj.fname
+//console.log(obj.intro)
+obj.intro()
