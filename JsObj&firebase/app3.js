b638ed181ce11e20196e7128d6eb58c08c331b54
Anumjawaid <anumjawaid99@gmail.com>
2020-10-27 22:30:44 +0500

    firebase function

---
+++ JsObj&firebase/app3.js
@@ -0,0 +1,11 @@
+
+function Save(){
+    var name=document.getElementById('name')
+var pw=document.getElementById('pw')
+    var student={
+        name:name.alue,
+        password:pw.value
+    }
+    console.log(student)
+
+}
