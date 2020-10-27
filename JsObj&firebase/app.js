82ab8318ba3c04c2f4fffe45e1ca2db708b3f696
Anumjawaid <anumjawaid99@gmail.com>
2020-10-27 23:43:04 +0500

    tracked

---
+++ JsObj&firebase/app.js
@@ -0,0 +1,31 @@
+obj={
+    fname:'Anum',
+    lname:'Jawaid',
+    Age :21,
+    intro:function (){
+        //document.write("MY Name IS Anum") Normally we will do that
+        document.getElementById('q').innerHTML="MY Name IS Anum"
+        console .log("My name is Anum")
+    },
+    hob:function(hobby){
+        document.getElementById('pq').innerHTML="My hobbie is "+hobby
+        console.log("My Hobbies are "+hobby)
+    }
+}
+console.log("if we direct call obj ==> ",obj)
+document.getElementById('o').innerHTML=("if we direct call obj ==> ",obj)
+console.log("accessing single name",obj.fname)
+document.getElementById('p').innerHTML=obj.fname
+//console.log(obj.intro)
+obj.intro()
+obj.list=[1,2,3,4,5]
+var a='see result in console'
+document.getElementById('r').innerHTML=a
+console.log("adding a property to already made object ==> ",obj)
+delete obj.Age;
+document.getElementById('rr').innerHTML=("see result in console");
+console.log("deleting Object.Age property ",obj)
+obj.hob('reading')
+
+
+
