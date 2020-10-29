bf64800fb869b8597653577b702db5a9c37c4a5e
Anumjawaid <anumjawaid99@gmail.com>
2020-10-29 23:45:44 +0500

    class components arr,variables

--- project/src/basicclasscomp.js
+++ project/src/basicclasscomp.js
@@ -6,10 +6,12 @@ class basic extends React.Component{
         let a="I am a Variable \n I am an Exception"
         let fn='Anum'
         let ln='Jawaid'
+        let arr=[1,2,3,4,5] //print in the form of pritn
         return(
         <div>
             <h1>From Basic Class Components<br></br>{a}</h1>
         <h3>{`${fn} ${ln}`}</h3>
+        <h2>{arr}</h2>
         </div>
         )
     }
