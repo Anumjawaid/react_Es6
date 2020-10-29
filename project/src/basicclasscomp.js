c0936583e8ccdbcc0596691db687184a0b3598c0
Anumjawaid <anumjawaid99@gmail.com>
2020-10-29 23:34:56 +0500

    variable using literal concatenation

--- project/src/basicclasscomp.js
+++ project/src/basicclasscomp.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 class basic extends React.Component{
     render(){
+        // rendering a variable in JSX
+        let a="I am a Variable \n I am an Exception"
+        let fn='Anum'
+        let ln='Jawaid'
         return(
         <div>
-            <h1>From Basic Class Components</h1>
+            <h1>From Basic Class Components<br></br>{a}</h1>
+        <h3>{`${fn} ${ln}`}</h3>
         </div>
         )
     }
