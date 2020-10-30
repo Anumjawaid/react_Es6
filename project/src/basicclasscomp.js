2f5a1e1ae759778f06dce8cc573d6785a3b8a7a5
Anumjawaid <anumjawaid99@gmail.com>
2020-10-30 06:37:54 +0500

    map functionality

--- project/src/basicclasscomp.js
+++ project/src/basicclasscomp.js
@@ -6,12 +6,23 @@ class basic extends React.Component{
         let a="I am a Variable \n I am an Exception"
         let fn='Anum'
         let ln='Jawaid'
-        let arr=[1,2,3,4,5] //print in the form of pritn
+        let arr=[1,2,3,4,5] //print in the form of print
+        let todo=[{'text':'namaz'},{'text':'parhai'}]
         return(
         <div>
             <h1>From Basic Class Components<br></br>{a}</h1>
         <h3>{`${fn} ${ln}`}</h3>
         <h2>{arr}</h2>
+        <h4>Todos</h4>
+        <ul>
+            {/* {todo.map((item,index)=>{ */}
+            {/* console.log(item) */}
+        {/* })} */}
+        {/* Another approach */}
+        {todo.map((item,index)=>{
+            return(<li key={index}>{item.text}</li>)
+        })}
+        </ul>
         </div>
         )
     }
