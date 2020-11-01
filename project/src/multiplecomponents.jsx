22414dccda1c732ada6107c7986ec8c5f2b7b1a3
Anumjawaid <anumjawaid99@gmail.com>
2020-11-01 13:47:53 +0500

    setstate rendering,binding

--- project/src/multiplecomponents.jsx
+++ project/src/multiplecomponents.jsx
@@ -1,27 +1,59 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Husky from './husky.JPG'
 
-class Main extends Component{
-    render(){
-        return(
+class Main extends Component {
+    render() {
+        return (
             <div>
-            <h1>I AM From Main Components</h1>
+                <h1>I AM From Main Components</h1>
             </div>
         )
     }
 
 }
-class Image extends Component{
-    render(){
-        return(
+class Image extends Component {
+    render() {
+        return (
             <div>
-            <h1>I am from Image Section</h1>
-            <img src={Husky} width='20' />
+                <h1>I am from Image Section</h1>
+                <img src={Husky} width='20' />
             </div>
         )
     }
 }
-export{
+class States extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: 'Anum Jawaid',
+            pw: '12346'
+        }
+        
+    }
+    set_name =()=>{
+        // console.log("Runnnning")
+        // console.log(this.state.name)
+        this.setState({
+            name:'AJ'
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>I Am from States</h1>
+                <h2>My Name is {this.state.name}</h2>
+                <input type='text' onChange={(e)=>this.setState({name:e.target.value})} placeholder='Enter your Name' />
+
+                <button  onClick={this.set_name} >Magic!!!!!!!!!</button>
+            </div>
+        )
+    }
+}
+
+export {
     Main,
-    Image
+    Image,
+    States
+  
 }
