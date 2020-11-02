48db041b29950bdd75d1415980eb8377d1a57ecf
Anumjawaid <anumjawaid99@gmail.com>
2020-11-02 17:20:28 +0500

    props,states,callbacks

--- project/src/Functional.js
+++ project/src/Functional.js
@@ -3,8 +3,10 @@ import React from 'react'
 // In ES6 form
 const start = (props) =>{
     return(
-
+<div>
     <h1> Begin To Learn React{props.name}</h1>
+    
+    </div>
     )
 }                  
 export default start
