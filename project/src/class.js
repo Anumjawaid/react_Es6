56ca274de1b8222e77545ef0348fbdd1f5c7e03f
Anumjawaid <anumjawaid99@gmail.com>
2020-11-02 17:20:47 +0500

    props,states,callbacks,class parent

--- project/src/class.js
+++ project/src/class.js
@@ -1,8 +1,17 @@
 import React ,{Component} from 'react'
-
+import {Main,Image,States} from './multiplecomponents'
 class Start1 extends Component {
+    get_data (props) {
+        console.log(props)
+      }
+      
     render(){
-    return <h1>I am  from class{this.props.name}</h1>
+    return(
+        <div>
+     <h1>I am  from class{this.props.name}</h1>
+    <Main send={this.get_data}></Main>
+    </div>
+    )
     }
     
 
