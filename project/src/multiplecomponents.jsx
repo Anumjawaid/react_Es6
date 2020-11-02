054dcd0a39321fee843296fdc346b6799368235b
Anumjawaid <anumjawaid99@gmail.com>
2020-11-02 17:21:12 +0500

    props,states,callbacks,class parent,..

--- project/src/multiplecomponents.jsx
+++ project/src/multiplecomponents.jsx
@@ -3,9 +3,11 @@ import Husky from './husky.JPG'
 
 class Main extends Component {
     render() {
+        console.log("props==",this.props.send)
         return (
             <div>
                 <h1>I AM From Main Components</h1>
+                <button onClick={()=>this.props.send('Abum')}>Send</button>
             </div>
         )
     }
@@ -32,22 +34,32 @@ class States extends Component {
         
     }
     set_name =()=>{
-        // console.log("Runnnning")
-        // console.log(this.state.name)
         this.setState({
             name:this.state.value
         })
 
+
+    }
+    handleChange(e){
+        // console.log(e.target.value)
+        this.setState({
+           [e.target.name]:e.target.value
+        })
     }
     render() {
         return (
             <div>
                 <h1>I Am from States</h1>
                 <h2>My Name is {this.state.name}</h2>
+                <h2>My Name is {this.state.pw}</h2>
+
                 {/* <input type='text' onChange={(e)=>this.setState({name:e.target.value})} placeholder='Enter your Name' ></input> this name changes the on real time */}
                 {/* <button  onClick={this.set_name} >Magic!!!!!!!!!</button> */}
-                <input type='text' onChange={(e)=>this.setState({value:e.target.value})} placeholder='Enter your Name' ></input>
-                <button onClick={this.set_name}></button>
+                {/* <input type='text' onChange={(e)=>this.setState({value:e.target.value})} placeholder='Enter your Name' ></input>for saving the value do this */}
+                <input name='name' onChange={(e)=>this.handleChange(e)} type='text' placeholder='Enter Your Name'></input>
+                <input name='pw' onChange={(e)=>this.handleChange(e)} type='text' placeholder='Enter Your Email'></input>
+                
+                <button onClick={this.set_name}>Enter</button>
             </div>
         )
     }
