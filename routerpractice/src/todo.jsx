3b0cc098f5e87917bb93b9b7463725f852c0af5f
Anumjawaid <anumjawaid99@gmail.com>
2020-11-03 23:39:05 +0500

    working fine till add

--- routerpractice/src/todo.jsx
+++ routerpractice/src/todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 class Todo extends React.Component{
     constructor(){
@@ -8,20 +8,21 @@ class Todo extends React.Component{
             value:''
         }
     }
-    Add=()=>{
+    Add = () => {
         this.state.todos.push(this.state.value)
-        console.log(this.state.todos)
-        // this.setState({
-        //     todos:this.state.todos
-        // })
+        // console.log(this.state.value)
+        this.setState({
+             todos:this.state.todos,
+             value:' '
+         })
     }
     render(){
-        let{todos,value}=this.state
+        let{todos,value}=this.state;
         return(
             <div>
                 <h1>Todo Application</h1>
-                <input type='text'  onChange={(e)=>this.setState({value:e.target.value})} placeholder='Enter Your Tasks Here' />
-                <button onClick={this.Add()}>Add Item</button>
+                <input type='text' value={value} onChange={(e)=>this.setState({value:e.target.value})} placeholder='Enter Your Tasks Here' />
+                <button onClick={this.Add}>Add Item</button>
                 <ul>
                     {todos.map((v,i)=>{
                         return <li key={i}>{v}</li>
