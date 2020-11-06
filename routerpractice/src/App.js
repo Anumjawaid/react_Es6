41992e0497fa275a036c359a5076b4b300c169ff
Anumjawaid <anumjawaid99@gmail.com>
2020-11-06 23:28:57 +0500

    state to props redux

--- routerpractice/src/App.js
+++ routerpractice/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 // import Todo from './todo'
 import Home from './containers/Home'
 import AppRouter from './containers/router'
+import { connect } from 'react-redux'
 
-function App() {
+class App extends React.Component {
+  render(){
+  console.log(this.props)
   return (
     <div className="App">
      {/* <Todo/> */}
      {/* <Home /> */}
      <AppRouter />
+     
     </div>
+    
   );
-}
-
-export default App;
+}}
+const mapStateToProps = (state) => ({
+  name:state.name,
+  age:state.Age
+  
+})
+export default connect(mapStateToProps,null)(App);
