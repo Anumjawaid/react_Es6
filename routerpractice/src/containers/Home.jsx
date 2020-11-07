8bc5c79d4a8bab2171cbd95839747ba38e6adfa8
Anumjawaid <anumjawaid99@gmail.com>
2020-11-07 13:30:32 +0500

    redux state set_data not working

--- routerpractice/src/containers/Home.jsx
+++ routerpractice/src/containers/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link } from "react-router-dom";
+import { connect } from 'react-redux'
+
 
 class Home extends React.Component{
     goto = () =>{
@@ -7,15 +9,27 @@ class Home extends React.Component{
 
     }
     render(){
-       
+        console.log("redux props",this.props)
         return(
             <div>
                 <h1>Header from Home</h1>
                 <h3>Why Not Working</h3>
                 {/* <Link to="/about"> About</Link> */}
                 <button onClick={this.goto}>About</button>
+     <button onClick={()=>this.props.set_data}>Set Data</button>
+            
             </div>
         )
     }
 }
-export default Home;
+const mik = (state) => ({
+    name:state.name,
+    age:state.Age
+    
+  })
+  const mapDispatchToProps = (dispatch) => ({
+    set_data : () => console.log("Hello")
+  
+  })
+  export default connect(mik,mapDispatchToProps)(Home);
+  
