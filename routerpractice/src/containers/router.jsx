fa112a0237598d9d3983a4cc205465f57c7f762a
Anumjawaid <anumjawaid99@gmail.com>
2021-09-13 13:36:56 +0500

    new links added

--- routerpractice/src/containers/router.jsx
+++ routerpractice/src/containers/router.jsx
@@ -4,6 +4,8 @@ import {
   Route} from "react-router-dom";
 import Home from './Home';
 import About from './about';
+import ContHome from '../containers/context/conthome'
+import Consumer  from "./context/consum";
 class AppRouter extends React.Component{
     render(){
         return(
@@ -11,6 +13,8 @@ class AppRouter extends React.Component{
                 <Router>
                     <Route exact path='/' component={Home} />
                     <Route path='/about' component={About} />
+                    <Route path='/conthome' component={ContHome} />
+                    <Route path='/consumer' component={Consumer} />
 
                 </Router>
             
